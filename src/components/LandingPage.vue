74d8607915722b4a448961c468625faedf41e89a
santhitak <santhita.sk@gmail.com>
2022-07-28 00:14:31 +0700

    refactor: pages

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="h-screen">
+    <div class="h-100 w-100 bg-orange"></div>
+  </section>
+</template>
---
+++ src/components/UsagePage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="ma-lg-14 ma-xs-2 text-center h-screen">
+    <div class="d-none d-lg-block d-md-none">
+      <p class="mx-8 mt-8 text-h2 font-weight-bold">Multi-Link Opener</p>
+    </div>
+    <div class="d-none d-md-block d-lg-none">
+      <p class="mx-8 mt-8 text-h3 font-weight-bold">Multi-Link Opener</p>
+    </div>
+    <div class="d-none d-sm-block d-md-none">
+      <p class="mx-8 mt-8 text-h4 font-weight-bold">Multi-Link Opener</p>
+    </div>
+    <div class="d-block d-sm-none">
+      <p class="mx-8 mt-8 text-h4 font-weight-bold">Multi-Link Opener</p>
+    </div>
+    <p class="ma-6 text-subtitle-1 text-orange-darken-2 font-weight-light">
+      A very shorter time to open all your copied links
+    </p>
+    <div>
+      <v-textarea
+        outlined
+        label="Paste your links here"
+        v-model="link"
+        class="my-8"
+      />
+    </div>
+    <v-card class="container" v-show="link">
+      <div class="ma-6">
+        <v-btn
+          class="ma-4"
+          v-for="data in splitLink"
+          :key="data"
+          :variant="checkHttps(data) ? '' : 'outlined'"
+          :class="
+            checkHttps(data)
+              ? 'bg-orange-darken-2 text-decoration-none mx-auto w-auto d-block'
+              : ' mx-auto d-block'
+          "
+          style="max-width: 70%"
+        >
+          <a
+            :href="checkHttps(data) ? data : null"
+            target="_blank"
+            class="text-truncate"
+            :class="checkHttps(data) ? 'text-white' : 'text-black'"
+          >
+            {{ checkHttps(data) ? data : data }}</a
+          >
+        </v-btn>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      link: "",
+      linkCount: 0,
+    };
+  },
+  computed: {
+    splitLink() {
+      return this.link.split(/\r?\n/);
+    },
+    countLink() {
+      for (let i = 0; i < this.splitLink.length; i++) {
+        this.splitLink[i].startsWith("https://")
+          ? this.linkCount + 1
+          : this.linkCount + 0;
+      }
+      return this.linkCount;
+    },
+  },
+  methods: {
+    checkHttps(data) {
+      return data.startsWith("https://") || data.startsWith("http://")
+        ? true
+        : false;
+    },
+  },
+});
+</script>
