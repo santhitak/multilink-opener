e2f493217417ec0e49f3f94ab9502898b6cae0dc
santhitak <santhita.sk@gmail.com>
2022-10-08 04:13:02 +0700

    add: id and how to

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <UsagePage />
+    <LandingPage />
+    <HowTo id="howto" />
+    <UsagePage id="usage" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import UsagePage from "@/components/UsagePage.vue";
+import LandingPage from "@/components/LandingPage.vue";
+import HowTo from "@/components/HowTo.vue";
 
 export default defineComponent({
   name: "HomeView",
-  components: { UsagePage },
+  components: { UsagePage, LandingPage, HowTo },
 });
 </script>
